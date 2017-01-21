e7a562f9547bff9afd3060aa7a90d3bfbde8ce69
Robert Krule <rkrule8638@gmail.com>
2017-01-21 06:17:44 +0000

    quick fix to get store working

--- src/index.php
+++ src/index.php
@@ -80,4 +80,4 @@ $mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : ''
 /* Run store or run website */
 $mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
 
-Mage::run($mageRunCode, $mageRunType);
+Mage::run('default');
