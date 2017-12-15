86142bd6f9100e0361f7f25dd324b457053f5aae
yuriiperevuznyk <bulldogyura11@gmail.com>
2017-12-15 08:34:45 -0800

    t2

--- app/code/local/Trial/controllers/IndexController.php
+++ app/code/local/Trial/controllers/IndexController.php
@@ -1,13 +1,14 @@
 <?php
 class Trial_IndexController extends Mage_Core_Controller_Front_Action {
     public function indexAction(){
-        echo "Hello Magento";
         
-        //$this->loadLayout ();
-        //$this->renderLayout ();
+        echo "Hello Magento";
     }
     
-    
+    public function advertisingAction() {
+    $this->loadLayout ();
+        $this->renderLayout ();    
+    }
         
 }
 
